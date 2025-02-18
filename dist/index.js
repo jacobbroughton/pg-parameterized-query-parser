f05153228f7454452b79efec006728f89d3aa969
Jacob Broughton <jlbroughton88@gmail.com>
2025-02-18 15:11:12 -0500

    transpiled and added a couple of error handling instances and set modifiedQuery to false

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,10 @@
 export function readQueryWithParams(query, params) {
-    console.log("----------------------");
     const paramsHashTable = {};
-    let modifiedQuery = query;
+    if (typeof query !== "string")
+        throw new Error("`query` must be a string");
+    if (params.constructor !== Array)
+        throw new Error("`params` must be an array");
+    let modifiedQuery = query || "";
     for (let i = 0; i < params.length; i++) {
         if (params[i] === null)
             params[i] = "null";
@@ -13,39 +16,8 @@ export function readQueryWithParams(query, params) {
         paramsHashTable[`$${i + 1}`] = params[i];
     }
     for (const paramKey in paramsHashTable) {
-        console.log(paramsHashTable[paramKey]);
         modifiedQuery = modifiedQuery.replaceAll(paramKey, paramsHashTable[paramKey]);
     }
     return modifiedQuery;
 }
-const initialQuery = `
-  select *
-    from
-        items a
-    inner join
-        users b on a.created_by_id = b.id
-    where lower(a.what_is_this) LIKE '%' || lower($1) || '%'
-      and a.price >= $2 
-    and (
-        ($3 > 0 AND a.price <= $3)
-        OR ($3 <= 0 OR $3 is null)  --Ensuring that if $3 is not greater than 0, it's ignored
-    )
-    and a.condition = any($4)
-    and a.shipping = any($5)
-    and a.negotiable = any($6)
-    and a.trades = any($7)
-    and a.is_deleted = $8
-`;
-const params = [
-    "Hello world",
-    0,
-    null,
-    ["Brand New", "Like New", "Used", "Heavily Used", "Not Functional"],
-    ["Willing to Ship", "Local Only"],
-    ["Firm", "OBO/Negotiable"],
-    ["Accepting Trades", "No Trades"],
-    false
-];
-console.log(readQueryWithParams(initialQuery, params));
-console.log("---------------------");
 //# sourceMappingURL=index.js.map
